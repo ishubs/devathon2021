fa68a9f2c7be15c323f839af892ebe4c4116e05a
divyamovva <divyamovva9@gmail.com>
2021-09-18 14:33:47 +0530

    faculty

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Widget.css";
 import WidgetContent from "./WidgetContent";
-
+import db, { auth, provider } from "../firebase";
 function Widget() {
+const [faculty, setfaculty] = useState([])
+  // useEffect(() => {
+  //   db.collection("faculty")
+  //     .onSnapshot((snapshot) =>
+  //       console.log(snapshot.doc.data())
+  //       // setPosts(
+  //       //   snapshot.map((doc) => ({
+  //       //     id: doc.id,
+  //       //     questions: doc.data(),
+  //       //   }))
+  //       // )
+  //     );
+  // }, []);
+
+  useEffect(() => {
+    db.collection("faculty")
+      .onSnapshot((snapshot) =>{
+          setfaculty(snapshot.docs.map((doc) => ({
+            id: doc.id,
+            email: doc.data().email,
+            photourl: doc.data().photourl == null? "": doc.data().photourl,
+          })))
+        }
+       
+      );
+  }, []);
   return (
     <div className="widget">
       <div className="widget__header">
         <h5>Faculty</h5>
       </div>
       <div className="widget__contents">
-        <WidgetContent />
+        {/* <WidgetContent /> */}
+        {faculty.map(i => (
+          <p>{i.email}</p>)
+        )}
       </div>
     </div>
   );
